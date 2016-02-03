eb6580ab9caab4b0b1d67e354cea9fe8340a5847
Lesley <lesleygamezcua@student.hartnell.edu>
2016-02-03 14:39:54 -0800

    First Commit

---
+++ project1.cpp
@@ -0,0 +1,15 @@
+//Name: Lesley Amezcua
+//Date: 02/03/16
+///Description: Project 1
+#include<iostream>
+#include<string>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World" << endl;
+
+
+return 0;
+}
